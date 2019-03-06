5b01a603fa87323efce75546134280b4fc1a8dcb
ayunas <ayunas@gmail.com>
2019-03-06 16:29:04 -0600

    wrote a simple closure without stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,13 +1,30 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function backpack(){
+    const gloves = 'warm';
+    
+    function shavingKit() {
+        console.log('these gloves are ' + gloves);
+    }
+    
+    shavingKit();
+}
+
+backpack();
+
+
+
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+    let counter = 0; 
+    
+    // Return a function that when invoked increments and returns a counter variable.
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
