252352253c04fb07bb2709517eee358d0680ff28
ayunas <ayunas@gmail.com>
2019-03-06 15:39:24 -0600

    finished callbacks assignment

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,27 +24,43 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+    cb(arr.length);
 }
+
+getLength(items, length => console.log('the length of the array is: ' + length));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+    cb(arr[arr.length-1]);
 }
+
+last(items, last => console.log(last));
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x+y);
 }
+
+sumNums(3,4, sum => console.log(`the sum is ${sum}`));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+    cb(x*y);
 }
+
+multiplyNums(3,4, result => console.log(`the result of multiplying the two numbers is: ${result}`))
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+    
+    list.includes(item) ? cb(true) : cb(false);
 }
+
+contains('yo-yo', items, isPresent => console.log('Is the item present? ' + isPresent));
+
 
 /* STRETCH PROBLEM */
 
@@ -52,4 +68,13 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+    let unique = items.filter( (item,i) => items.indexOf(item) >= i);  
+    cb(unique);
+    //because indexOf(item) looks for the first occurrence of the item, if that happens to be less than the current index, that means there is more than one occurrence of that item.  so only return the item index that is greater than or = to current index.  
 }
+
+
+removeDuplicates(items, (array) => console.log(array));
+
+
+
